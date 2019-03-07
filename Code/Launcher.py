c41707d2058cae98a33825663c9764a35f9f90c2
Kyloren3600 <kyloren3600@gmail.com>
2019-03-07 12:44:27 +0100

    Update

--- Code/Launcher.py
+++ Code/Launcher.py
@@ -8,30 +8,24 @@ from distutils.dir_util import copy_tree
 import os
 import urllib.request, json
 
-
-def download_update(self):
+def download_update():
 	url = "https://github.com/KyloRen3600/KyloBot/archive/master.zip"
 	urllib.request.urlretrieve(url, "Temp/Update.zip")
 	zip_ref = zipfile.ZipFile("Temp/Update.zip", 'r')
 	zip_ref.extractall("Temp")
 	zip_ref.close()
-	path = './Temp/KyloBot-master'
-	dst = ""
+	path = './Temp/KyloBot-master/Code'
+	dst = "./"
 	files = os.listdir(path)
 	for file in files:
 		try:
-			copyfile('./Temp/KyloBot-master/{0}'.format(file), "{1}".format(dst, file))
-			os.remove('./Temp/KyloBot-master/{0}'.format(file))
+			copyfile('./Temp/KyloBot-master/Code/{0}'.format(file), "{1}".format(dst, file))
+			os.remove('./Temp/KyloBot-master/Code/{0}'.format(file))
 		except:
-			copy_tree('./Temp/KyloBot-master/{0}'.format(file), "{1}".format(dst, file))
-			rmtree('./Temp/KyloBot-master/{0}'.format(file))
+			copy_tree('./Temp/KyloBot-master/Code/{0}'.format(file), "{1}".format(dst, file))
+			rmtree('./Temp/KyloBot-master/Code/{0}'.format(file))
 	rmtree('./Temp/KyloBot-master')
 	os.remove('./Temp/Update.zip')
-
-
-
-
-
 
 with urllib.request.urlopen("https://raw.githubusercontent.com/KyloRen3600/KyloBot/master/Code/Version.json") as url:
 	data = json.loads(url.read().decode())
@@ -42,18 +36,8 @@ with open("Version.json", 'r') as f:
 	local_version = data["Version"]
 
 if version != local_version:
-	print("Mise à jour disponible")
 	ask = askyesno("Mise à jour disponible !", "Voulez vous l'installer ?")
 	if ask == True:
-		print("Installation")
+		download_update()
 
-
-else:
-	print("A jour")
-
-
-
-
-
-
-#import KyloBot
+import KyloBot
